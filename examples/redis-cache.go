63a135643b3bc1e0837c3824db13f2a365b41be5
chloyka <chloyka@gmail.com>
2023-04-19 19:36:05 -0300

    docs: add example with redis middleware

--- examples/redis-cache.go
+++ examples/redis-cache.go
@@ -2,15 +2,19 @@ package examples
 
 import (
 	"github.com/chloyka/ginche"
+	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 )
 
 // Redis example
 func main() {
-	store := ginche.NewRedisAdapter(&redis.Options{
+	r := gin.New()
+	// No need to specify the options, just pass nil
+	// No exclusion rules, default key is (Request.URL.Path)
+	r.Use(ginche.Middleware(ginche.NewRedisAdapter(&redis.Options{
 		Addr: "localhost:6379",
+	}), nil))
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
 	})
-	key := "test_key"
-	store.Set(&key, "test_value")
-	store.Get("test_key")
 }
